0c5670e6edc4bfcfbc270b5d583a0a3fdb0d2374
Enrico Visentini <enricovise@gmail.com>
2014-03-22 14:58:13 +0100

    displayIn and flipIn tests

--- test.js
+++ test.js
@@ -13,3 +13,17 @@ function get_first_card()
 	var card = (new List()).get(0);
 	return card.front == "Hello. (formal) " && card.back == "Salve. (sAH-lveh)";
 }
+
+function displayIn_fills_in_element_innerHTML(frontString, backString, element)
+{
+	var card = new Card(frontString, backString);
+	card.displayIn(element);
+	return element.innerHTML == card.front;	
+}
+
+function flipIn_fills_in_element_innerHTML(frontString, backString, element)
+{
+	var card = new Card(frontString, backString);
+	card.flipIn(element);
+	return element.innerHTML == card.back;	
+}
