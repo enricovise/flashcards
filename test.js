98935207f4717df07473f1e465adcdbc917e2e27
Enrico Visentini <enricovise@gmail.com>
2014-03-22 13:06:28 +0100

    flip test

--- test.js
+++ test.js
@@ -2,4 +2,10 @@ function display_returns_front(frontString, backString)
 {
 	var card = new Card(frontString, backString);
 	return card.display() == frontString;
-};
+}
+
+function flip_returns_back(frontString, backString)
+{
+	var card = new Card(frontString, backString);
+	return card.flip() == backString;
+}
